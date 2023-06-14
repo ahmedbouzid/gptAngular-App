1bcbe8ecb651a9ce60e8e8ebc93297ff5d5771cb
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-14 21:07:40 +0200

    configure routing && import bootstrap //-icons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { ChatGptComponent } from './chat-gpt/chat-gpt.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: IndexComponent },
+  { path: 'gpt', component: ChatGptComponent },
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'chatGpt-Angular-app';
+  constructor(private router: Router) {}
+  action = [
+    { route: '/index', titre: 'index', icon: 'house' },
+    { route: '/gpt', titre: 'gpt', icon: 'person' },
+  ];
+
+  handleAction(action: any) {
+    this.router.navigate([action.route]);
+  }
 }

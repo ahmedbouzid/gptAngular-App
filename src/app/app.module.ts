0d824ccad4be54eead8da876acb39487ab058d73
AhmedInes <ahmed.bouzid.ines@gmail.com>
2023-06-14 20:42:24 +0200

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { ChatGptComponent } from './chat-gpt/chat-gpt.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndexComponent,
+    ChatGptComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/chat-gpt/chat-gpt.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-gpt',
+  templateUrl: './chat-gpt.component.html',
+  styleUrls: ['./chat-gpt.component.scss']
+})
+export class ChatGptComponent {
+
+}
